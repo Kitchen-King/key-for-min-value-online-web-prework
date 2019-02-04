72997ad464014458c887d9015984756bc9f7375b
Kyle Spencer <spencerk2@vcu.edu>
2019-02-04 03:04:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,11 @@
 
 def key_for_min_value(name_hash)
 
-current_value = name_hash.values[0]
   
   name_hash.each do
     	|key, value|
     	
-            if value < current_value
+            if current_value == nil || value < current_valuevalue < current_value
             then current_value = value
             end
         
